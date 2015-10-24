7bdccd0745d24829c41818413c5c155acd76098e
souravsaha1605 <souravsaha1605@gmail.com>
2015-10-24 13:37:59 +0530

    Image size has been retrieve

--- applications/test3/controllers/project_demo.py
+++ applications/test3/controllers/project_demo.py
@@ -1,7 +1,6 @@
 from binascii import a2b_base64
 from PIL import Image
 import os
-#import cStringIO
 import StringIO
 def image_insert():
     name_image=request.vars.name_image
@@ -21,43 +20,43 @@ def image_insert2():
     dataList = data.split(',')
     imageString = dataList[1]
     size = len(imageString)*3/4
-    session.new = [size]
-    imageString.replace(" ", "+")
-    session.new.append(imageString)
-    imageDecode = base64.b64decode(imageString)
-    #session.new.append(imageDecode)
-    imageFileWrite = open(name_image, 'wb')
-    imageFileWrite.write(imageDecode)
-    imageFileWrite.close()
-    imageFileOpen = open(name_image,'rb')
-    im=Image.open(name_image)
-    (width,height)=im.size
-    name_image_dupli=name_image.split('.')[0]
-    name_image_992558=name_image_dupli+"992*558.jpeg"
-    y = db.imagestore.insert(name = name_image, picture = imageFileOpen )
-    #os.remove(name_image)
-    # Changing image size of the uploaded image
-    imageUploaded = db(db.imagestore.id == y).select().first()
-    imageURL = URL('default', 'download', args=[imageUploaded.picture],host=True)
-    imageURL2=imageURL.split('.')
-    imageURL3=imageURL2[5]+'.'+imageURL2[6]+'.'+imageURL2[7]
-    import urllib, cStringIO
-    file = cStringIO.StringIO(urllib.urlopen(imageURL).read())
-    newImage = Image.open(file)
-    #session.new.append(imageURL3)
-    (width,height) = newImage.size
-    session.new.append(width)
-    size=(992,558)
-    newImage.thumbnail(size,Image.ANTIALIAS) 
-    newImage.save(name_image_992558,'jpeg')
-    
-    #imageDupliFileWrite95=open(name_image_992558, 'wb')
-    #imageDupliFileWrite95.write(file_like)
-    #imageDupliFileWrite95.close()
-    imageDupliFileOpen=open(name_image_992558, 'rb')
-    db(db.imagestore.id == y).update(picture1=imageDupliFileOpen)
-    imageDupliFileOpen.close()
-    return newImage
+    if size < 4893970:
+        session.new = [size]
+        session.new.append(size)
+        imageString.replace(" ", "+")
+        #session.new.append(imageString)
+        imageDecode = base64.b64decode(imageString)
+        #session.new.append(imageDecode)
+        imageFileWrite = open(name_image, 'wb')
+        imageFileWrite.write(imageDecode)
+        imageFileWrite.close()
+        imageFileOpen = open(name_image,'rb')
+        im=Image.open(name_image)
+        (width,height)=im.size
+        name_image_dupli=name_image.split('.')[0]
+        name_image_992558=name_image_dupli+"992*558.jpeg"
+        y = db.imagestore.insert(name = name_image, picture = imageFileOpen )
+        os.remove(name_image)
+        # Changing image size of the uploaded image
+        imageUploaded = db(db.imagestore.id == y).select().first()
+        imageURL = URL('default', 'download', args=[imageUploaded.picture],host=True)
+        import urllib, cStringIO
+        file = cStringIO.StringIO(urllib.urlopen(imageURL).read())
+        newImage = Image.open(file)
+        #session.new.append(imageURL3)
+        (width,height) = newImage.size
+        session.new.append(width)
+        size=(992,558)
+        newImage.thumbnail(size,Image.ANTIALIAS) 
+        newImage.save(name_image_992558,'jpeg')
+        imageDupliFileOpen=open(name_image_992558, 'rb')
+        db(db.imagestore.id == y).update(picture992558=imageDupliFileOpen)
+        imageDupliFileOpen.close()
+        os.remove(name_image_992558)
+        return 'inserted'
+    else:
+        return 'Failed to insert, image greater than 5MB'
+
 
 
 def image_upload():
